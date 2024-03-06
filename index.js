2ce6f88a417036e8f5e4563376f59f3933f1b741
“CollinABullock” <collin@collinbullock.com>
2024-03-06 12:23:01 -0600

    trying to get post route to work

--- index.js
+++ index.js
@@ -1,45 +1,59 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const uuid = require("uuid"); // Import the uuid package to generate unique IDs
+
+app.use(bodyParser.json());
+
 let port = process.env.PORT || 3000;
-const netflixData = require("./netflixArray.json")
-const maxData = require("./maxArray.json")
-const primeData = require("./primeArray.json")
-const huluData = require("./huluArray.json")
-const peacockData = require("./peacockArray.json")
+let importData = []; // Define an array to store the posted data
+
+const netflixData = require("./netflixArray.json");
+const maxData = require("./maxArray.json");
+const primeData = require("./primeArray.json");
+const huluData = require("./huluArray.json");
+const peacockData = require("./peacockArray.json");
 
 app.get("/", (req, res) => {
-  res.send ("Movies are gay")
-})
+  res.send("Movies are gay");
+});
 
 app.get("/netflixArray", (req, res) => {
-  res.send (netflixData)
-})
+  res.send(netflixData);
+});
 
 app.post("/netflixArray", (req, res) => {
   const newItem = req.body;
 
-  netflixData.push(newItem);
+  // Generate a unique item number using UUID
+  const newItemNumber = uuid.v4(); // Generate a version 4 UUID
+
+  // Add the generated item number to the newItem object
+  newItem.item = newItemNumber;
+
+  // Push the new item into the importData array
+  importData.push(newItem);
 
   // Send a response indicating success
   res.status(201).json({ message: "You just added a movie", item: newItem });
 });
 
 app.get("/maxArray", (req, res) => {
-  res.send (maxData)
-})
+  res.send(maxData);
+});
 
 app.get("/primeArray", (req, res) => {
-  res.send (primeData)
-})
+  res.send(primeData);
+});
 
 app.get("/huluArray", (req, res) => {
-  res.send (huluData)
-})
+  res.send(huluData);
+});
 
 app.get("/peacockArray", (req, res) => {
-  res.send (peacockData)
-})
+  res.send(peacockData);
+});
 
 app.listen(port, () => {
-  console.log(`We're all listening on port ${3000} and you are gay but also you're cool and smart and will achieve great things`);
-})
+  console.log(`Server is running on port ${port}`);
+});
