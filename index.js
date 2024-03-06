fe1683d08e5a094a5b1a6f6429bc1e4b191eb47f
“CollinABullock” <collin@collinbullock.com>
2024-03-06 11:47:33 -0600

    got the other json endpoints working

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 let port = process.env.PORT || 3000;
 const netflixData = require("./netflixArray.json")
 const maxData = require("./maxArray.json")
+const primeData = require("./primeArray.json")
+const huluData = require("./huluArray.json")
+const peacockData = require("./peacockArray.json")
 
 app.get("/", (req, res) => {
   res.send ("Movies are gay")
@@ -14,6 +17,18 @@ app.get("/netflixArray", (req, res) => {
 
 app.get("/maxArray", (req, res) => {
   res.send (maxData)
+})
+
+app.get("/primeArray", (req, res) => {
+  res.send (primeData)
+})
+
+app.get("/huluArray", (req, res) => {
+  res.send (huluData)
+})
+
+app.get("/peacockArray", (req, res) => {
+  res.send (peacockData)
 })
 
 app.listen(port, () => {
