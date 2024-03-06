8dbcf8e3ab06b1ecf2c6ae1ea95788530aae5e20
“CollinABullock” <collin@collinbullock.com>
2024-03-06 12:36:43 -0600

    deploy

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const uuid = require("uuid"); // Import the uuid package to generate unique IDs
 app.use(bodyParser.json());
 
 let port = process.env.PORT || 3000;
-let importData = []; // Define an array to store the posted data
 
 const netflixData = require("./netflixArray.json");
 const maxData = require("./maxArray.json");
@@ -22,24 +21,21 @@ app.get("/netflixArray", (req, res) => {
   res.send(netflixData);
 });
 
-// app.post("/netflixArray", (req, res) => {
-//   const newItem = req.body;
+app.post("/netflixArray", (req, res) => {
+  const newItem = req.body;
 
-//   // Generate a unique item number using UUID
-//   const newItemNumber = uuid.v4(); // Generate a version 4 UUID
+  // Generate a unique item number using UUID
+  const newItemNumber = uuid.v4(); // Generate a version 4 UUID
 
-//   // Add the generated item number to the newItem object
-//   newItem.item = newItemNumber;
+  // Add the generated item number to the newItem object
+  newItem.item = newItemNumber;
 
-//   // Push the new item into the importData array
-//   importData.push(newItem);
+  // Update the netflixData array with the new item
+  netflixData.push(newItem);
 
-//   // Update the netflixData array with the new item
-//   netflixData.push(newItem);
-
-//   // Send a response indicating success
-//   res.status(201).json({ message: "You just added a movie bucko", item: newItem });
-// });
+  // Send a response indicating success
+  res.status(201).json({ message: "You just added a movie bucko", item: newItem });
+});
 
 app.get("/maxArray", (req, res) => {
   res.send(maxData);
